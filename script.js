b5c75721c92ffd35921f5f2a40831bf70ec8bf02
Nick <nickmagi24@gmail.com>
2021-08-30 12:22:43 -0400

    Compplay and play round function

--- script.js
+++ script.js
@@ -35,11 +35,38 @@ buttons.forEach((button)=>{button.addEventListener('click', ()=>{
 })
 
 
+ // Computer makes a random play
+function computerPlay() {
+    let compChoice = Math.floor(Math.random() * 3) // Generates random num. 0 through 3.
+    if (compChoice < 1) {
+        compChoice = "rock";
+    } else if (compChoice = 2) {
+        compChoice = "paper";
+    } else {
+        compChoice = "scissors";
+    }
+    return compChoice;
+ }
 
+//console.log(computerPlay());
 
+function playRound() {
+    let win_array = [[0, 2, 1],
+                    [1, 0, 2],
+                    [2, 1, 0]];
 
-
-
+    let result = win_array[playerChoiceInt][computerChoiceInt];
+    if (result == 0) {
+        output.textContent = `It's a tie! You chose ${playerChoice} and The computer chose ${computerChoice.Value}`;
+    }
+    else if (result == 1) {
+        output.textContent = `You Won! You chose ${playerChoice} and The computer chose ${computerChoice.Value}`;
+        playerScore++;
+    } else if (result == 2) {
+        output.textContent = `You lost! You chose ${playerChoice} and The computer chose ${computerChoice.Value}`;
+        compScore++;
+    }
+}
 
 
 
